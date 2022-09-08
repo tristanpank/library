436a39d50827b4d833bdd352f3983fd63ce51037
tristanpank <tristanpank@gmail.com>
2022-09-07 21:32:52 -0400

    Add Input for Author and Pages

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let myLibrary = [];
 
-function Book(title) {
+function Book(title, author, pages) {
     this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
 function addBookToLibrary(book) {
@@ -10,9 +12,13 @@ function addBookToLibrary(book) {
 
 function collectInput() {
     let newBookTitle = document.getElementById('title').value;
-    let newBook = new Book(newBookTitle);
+    let newBookAuthor = document.getElementById('author').value;
+    let newBookPages = document.getElementById('pages').value;
+    let newBook = new Book(newBookTitle, newBookAuthor, newBookPages);
     addBookToLibrary(newBook);
     console.log(myLibrary[0].title);
+    console.log(myLibrary[0].author);
+    console.log(myLibrary[0].pages);
 }
 
 let submitButton = document.getElementById('submit-form');
